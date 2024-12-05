7b5145358d3750ec32a66551fd5b94f73fedb614
Nida Deshmukh <nidadeshmukh7@gmail.com>
2024-12-05 12:35:27 +0530

    first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.chat_models import ChatOpenAI
+from langserve import add_routes
+import uvicorn
+import os
+from langchain_community.llms import Ollama
+from dotenv import load_dotenv
